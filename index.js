7ed12d532cc209981bbd78da0b6943f50a8b615f
Your Name <minajmiha02@gmail.com>
2025-09-10 23:48:05 +0600

    mongodb keys

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT||5000
-require('dotenv').config()
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,16 +17,25 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCallection = client.db("restaurant").collection("menu");
+    const reviewsCallection = client.db("restaurant").collection("reviews");
+    app.get("/menu", async (req, res) => {
+      const result = await menuCallection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -37,11 +43,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',async(req,res)=>{
-    res.send('restaurant-server is running')
-})
-app.listen(port,()=>{
-    console.log(`restaurant-server is running on port ${port}`)
-})
+app.get("/", async (req, res) => {
+  res.send("restaurant-server is running");
+});
+app.listen(port, () => {
+  console.log(`restaurant-server is running on port ${port}`);
+});
