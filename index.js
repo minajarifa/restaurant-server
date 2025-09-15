5dc9e275d2b2a1530d6a23f640d030cef579978c
Your Name <minajmiha02@gmail.com>
2025-09-15 19:28:00 +0600

    error solved

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 // const cookieParser = require("cookie-parser");
@@ -7,14 +7,14 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(
-  // cors({
-  //   origin: [
-  //     "http://localhost:5173",
-  //     "https://restaurant-6febb.web.app",
-  //     "https://restaurant-6febb.firebaseapp.com",
-  //   ],
-  //   credentials: true,
-  // })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://restaurant-6febb.web.app",
+      "https://restaurant-6febb.firebaseapp.com",
+    ],
+    credentials: true,
+  })
 );
 
 // middleware
