930a07496d01ab30861b4ac2ec7ae92fd8bcab20
Your Name <minajmiha02@gmail.com>
2025-09-22 19:51:31 +0600

    cartCollection

--- index.js
+++ index.js
@@ -39,16 +39,31 @@ async function run() {
 
     const menuCollection = client.db("restaurant").collection("menu");
     const reviewsCollection = client.db("restaurant").collection("reviews");
+    const cartCollection = client.db("restaurant").collection("carts");
+
+    // menuCollection
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
-    // Send a ping to confirm a successful connection
+    // reviewsCollection
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
-  
+    // cartCollection
+   
+
+    app.get("/carts", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
